cd9373a993e0baa7df575cd2435de8ae0d16b3e9
신준혁 <oridori2705@naver.com>
2024-06-15 03:07:21 +0900

    refactor: useFetchCourseList 내부 함수 모듈화

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,10 @@
+import { Dispatch, SetStateAction } from 'react'
+
+import { categoriesObject } from '~/constants/course'
+import { Filters } from '~/constants/filter'
 import { ParameterRoot } from '~/types/api'
 import { ResponseDataType } from '~/types/data'
+import { generateAPIParams } from '~/utils'
 
 const baseURL = import.meta.env.VITE_API_BASE_URL
 
@@ -21,4 +26,25 @@ export const fetchCourseList = async (
     throw new Error('네트워크 응답이 올바르지 않습니다.')
   }
   return response.json()
+}
+
+export const loadCourseList = async (
+  filters: Filters,
+  offset: number,
+  setCourseList: Dispatch<SetStateAction<ResponseDataType | null>>,
+  setIsLoading: Dispatch<SetStateAction<boolean>>,
+  setError: Dispatch<SetStateAction<string | null>>
+) => {
+  const apiParams = generateAPIParams(filters, categoriesObject)
+  try {
+    setIsLoading(true)
+    const data = await fetchCourseList(apiParams, 20 * (offset - 1), 20)
+    setCourseList(data)
+  } catch (error) {
+    console.error('Fetch 에러:', error)
+    setError('데이터를 불러오는 중 문제가 발생했습니다.')
+    setCourseList(null)
+  } finally {
+    setIsLoading(false)
+  }
 }
--- src/hooks/useFetchCourseList.ts
+++ src/hooks/useFetchCourseList.ts
@@ -1,11 +1,10 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 
-import { fetchCourseList } from '~/api'
-import { categoriesObject } from '~/constants/course'
-import { DEFAULT_COUNT, Filters, filtersInitialState } from '~/constants/filter'
+import { loadCourseList } from '~/api'
+import { Filters, filtersInitialState } from '~/constants/filter'
 import { ResponseDataType } from '~/types/data'
-import { generateAPIParams, parseFiltersFromQuery } from '~/utils'
+import { parseFiltersFromQuery } from '~/utils'
 
 const useFetchCourseList = () => {
   const location = useLocation()
@@ -49,27 +48,7 @@ const useFetchCourseList = () => {
   }, [])
 
   useEffect(() => {
-    const loadCourseList = async () => {
-      const apiParams = generateAPIParams(filters, categoriesObject)
-
-      try {
-        setIsLoading(true)
-        const data = await fetchCourseList(
-          apiParams,
-          20 * (offset - 1),
-          DEFAULT_COUNT
-        )
-        setCourseList(data)
-      } catch (error) {
-        console.error('Fetch 에러:', error)
-        setError('데이터를 불러오는 중 문제가 발생했습니다.')
-        setCourseList(null)
-      } finally {
-        setIsLoading(false)
-      }
-    }
-
-    loadCourseList()
+    loadCourseList(filters, offset, setCourseList, setIsLoading, setError)
   }, [filters, offset])
 
   useEffect(() => {
